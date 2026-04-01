811bd59c0c3a5d16740305d87ca8720cc2ec3030
Nicholas823215 <nfed0104@uni.sydney.edu.au>
2026-04-01 13:27:02 +1100

    Created simplitial collapses
    Still some buggs to weed out

--- Morse.py
+++ Morse.py
@@ -1,151 +1,55 @@
 import pygame
 import sys
 import random
+from Simplitial_Complises import *
 
 pygame.init()
 pygame.font.init()
 screen_width = 800
-screen_height = 600
+screen_height = 800
 center =(screen_width/2,screen_height/2)
 screen = pygame.display.set_mode((screen_width, screen_height))
+font = pygame.font.SysFont("newtimesroman",30)
 
-class Set:
-    elements = {}
+SimC = SC([Set(["good"]),Set([2]),Set(["good",2]),Set([4]),Set([3,2]),Set([3,2,4]),Set(["good",4]),Set([4,2]),Set([3]),Set([5]),Set([5,3]),Set([3,4])])
 
-    def __init__(self,set1: list,create_Power_set = True):
+SimC = SC(SimC.Simplisial_Collaps())
+SimC = SC(SimC.Simplisial_Collaps())
+SimC = SC(SimC.Simplisial_Collaps())
+SimC = SC(SimC.Simplisial_Collaps())
+SimC = SC(SimC.Simplisial_Collaps())
+SimC.Define_graphical_cords()
 
-        for i in self.elements:
-            self.elements[i] = True
+print(SimC.highest_dim)
+def draw(draw_points = False):
+    try:
+        for i in SimC.dim_simpl[2]:
+            n = SimC.border(i)
+            h = {}
+            j =[]
+            for i in n:
+                h[SimC.border(i)[0]] = 1
+                h[SimC.border(i)[1]] = 1
+            for i in h:
+                j.append(i)
+            pygame.draw.polygon(screen,(0,0,255),[SimC.cords[j[0]],SimC.cords[j[1]],SimC.cords[j[2]]])
+    except: pass
+    try:
+        for i in SimC.dim_simpl[1]:
+            n = SimC.border(i)
+            pygame.draw.line(screen,(0,255,0),SimC.cords[n[0]],SimC.cords[n[1]],3)
+    except: pass
+    try:
+        for i in SimC.dim_simpl[0]:
+            if draw_points:
+                pygame.draw.circle(screen,(255,0,0),SimC.cords[i],5)
+            text_surface = font.render(str(i), True, (0,0,0))
+            text_render = text_surface.get_rect(center = SimC.cords[i])
+            screen.blit(text_surface, text_render)
+    except: pass
 
-        if create_Power_set:
-            self.CreatePowerSet()
-
-    def CreatePowerSet(self):
-        self.PowerSet.append([])
-        length = len(self.elements)
-        for i in self.elements:
-            if not([i] in self.PowerSet):
-                self.PowerSet.append([i])
-        if length >= 2:
-            for i in self.elements:
-                for n in self.elements:
-                    if n != i and not([n,i] in self.PowerSet or [i,n] in self.PowerSet):
-                        self.PowerSet.append([i,n])
-        if length >= 3:
-            for i in self.elements:
-                for n in self.elements:
-                    for o in self.elements:
-                        Is_alredy_there = [i,n,o] in self.PowerSet or [i,o,n] in self.PowerSet or [n,i,o] in self.PowerSet or [n,o,i] in self.PowerSet or [o,i,n] in self.PowerSet or [o,n,i] in self.PowerSet
-                        if n != i and i != o and n != o and not(Is_alredy_there):
-                            self.PowerSet.append([i,n,o])
-
-    def IsIn(self, it):
-        if self.elements.get(it) == None:
-            return False
-        return True
     
-    def IsSubSet(self, it):
-        Is = []
-        for i in it.elements:
-            Is.append(False)
-        
-        for i in range(len(it.elements)):
-            for n in self.elements:
-                if n == it[i]:
-                    Is[i] = True
-        
-        if False in Is:
-            return False
-        return True
     
-    def add(self, i):
-        self.elements[i] = 1
-
-    def remove(self, i):
-        for n in range(len(self.elements)):
-            if i == n:
-                self.element.pop(n)      
-
-    PowerSet = []
-
-def Union(set1: Set, set2: Set) -> Set:
-    return Set(set1.elements + set2.elements)
-
-def Intsec(set1: Set, set2: Set) -> Set:
-    end_list = []
-    for i in set1.elements:
-        if set2.IsIn(i):
-            end_list.append(i)
-    return Set(end_list)
-
-class SC:
-    def __init__(self,the: list[Set],record_dim_info = True):
-        list1 = []
-        for i in the:
-            list1 += i.PowerSet
-        self.elements = Set(list1).elements
-        if record_dim_info:
-            for i in self.elements:
-                if len(i) == 1:
-                    self.dim0.append(i)
-                if len(i) == 2:
-                    self.dim1.append(i)
-                if len(i) == 3:
-                    self.dim2.append(i)
-        
-    elements = None
-    dim0 = []
-    dim1 = []
-    dim2 = []
-
-def statistical_funct(boundury1 = int(screen_height/2), boundury2 = int(screen_width/2),spread = 100):
-    return [random.randint(boundury1-100,boundury1+100),random.randint(boundury2-100,boundury2+100)]
-
-def Cords_Morse_Function(sc: SC):
-    cords = []
-    for i in sc.dim0:
-        cords.append([statistical_funct(),i])
-    return cords
-
-def InverseList(elemnt, list1: list):
-    for i in range(len(list1)):
-        if list1[i] == elemnt:
-            return i
-    return None
-
-d = Set([1,2,3])
-u = Set([1,2,10])
-print(Intsec(d,u).elements)
-sc = SC([d,u])
-graph = Cords_Morse_Function(sc)
-print(Cords_Morse_Function(sc))
-
-def Unpack(lest1):
-    final = []
-    for i in lest1:
-        final += i
-    return final
-
-font = pygame.font.SysFont(None,30)
-text_surface = []
-text_rect = []
-for i in graph:
-    text_surface.append(font.render(str(i[1]),True,(0,0,0)))
-    text_rect.append(text_surface[-1].get_rect())
-    text_rect[-1].center = (i[0])
-
-graph0 = Unpack(graph)
-graph1 = []
-print(graph1)
-print(InverseList(2,d.elements))
-for i in sc.dim1:
-    print(graph0)
-    Index1 = InverseList([i[0]],graph0)
-    Index2 = InverseList([i[1]],graph0)
-    Index1 -= 1
-    Index2 -= 1
-    graph1.append([graph0[Index1],graph0[Index2]])
-print(graph1)
 
 while True:
     for event in pygame.event.get():
@@ -154,11 +58,7 @@ while True:
             sys.exit()
     screen.fill((255,255,255))
 
-    for i in range(len(graph)):
-        #pygame.draw.circle(screen,(0,0,255),graph[i][0],2)
-        screen.blit(text_surface[i], text_rect[i])
-    for i in graph1:
-        pygame.draw.line(screen,(255,0,0),i[0],i[1],5)
+    draw()
 
     pygame.display.flip()
     pygame.time.wait(1)
--- Simplitial_Complises.py
+++ Simplitial_Complises.py
@@ -1,13 +1,16 @@
+import random
+import math
 
 class Set:
-    def __init__(self, list1):
+    def __init__(self, list1 = []):
         self.elements = {}
-        try:
-            list1.__iter__()
-        except:
-            raise TypeError(f"{list1} is NOT an itterable type")
-        for i in list1:
-            self.elements[i] = 1
+        if list1 != []:
+            try:
+                list1.__iter__()
+            except:
+                raise TypeError(f"{list1} is NOT an itterable type")
+            for i in list1:
+                self.elements[i] = 1
 
     def __str__(self):
         return str(list(self.elements.keys()))
@@ -17,12 +20,24 @@ class Set:
         return len(self.element())
 
     def issubsetof (self, set2):
+        if self.__len__() == 0:
+            return True
         for i in self:
             if not(set2.isin(i)):
                 return False
         return True
+    
+    def issupersetof(self, set2):
+        if set2.__len__() == 0:
+            return False
+        for i in set2:
+            if not(self.isin(i)):
+                return False
+        return True
 
     def isin(self,element):
+        if self.__len__() == 0:
+            return False
         for i in self:
             if element == i:
                 return True
@@ -30,8 +45,14 @@ class Set:
     
     def element(self):
         return list(self.elements.keys())
+    
+    def add(self, element):
+        self.elements[element] = 1
+
 
     def __sub__(self, other):
+        if self.__len__() == 0:
+            return Set()
         long = []
         for i in self:
             if other.isin(i):
@@ -40,6 +61,8 @@ class Set:
         return Set(long)
     
     def __truediv__(self, other):
+        if self.__len__() == 0:
+            return Set()
         long = []
         for i in self:
             if not(other.isin(i)):
@@ -47,9 +70,13 @@ class Set:
         return Set(long)
 
     def __add__(self, set2):
+        if self.__len__() == 0:
+            return set2
         return Set(self.element() + set2.element())
     
     def __mul__(self, other):
+        if self.__len__() == 0:
+            return Set()
         list1 = []
         for i in self:
             for n in other:
@@ -57,6 +84,12 @@ class Set:
         return Set(list1)
     
     def __eq__(self, value):
+        if self.__len__() == 0 and value.__len__() == 0:
+            return True
+        elif self.__len__() == 0:
+            return False
+        elif value.__len__() == 0:
+            return False
         for i in self:
             if not(value.isin(i)):
                 return False
@@ -83,11 +116,7 @@ class Set:
                 list1.append([i])
             return list1
 
-
-
-
-
-    def power_set(self):
+    '''def power_set(self):
         list1 = self._itterfor(len(self))
 
         for i in range(len(list1)): #makes sure that we do not have elements like [1,1,1,1,...,1]
@@ -97,7 +126,7 @@ class Set:
                     is_the_same = False
             if is_the_same:
                 list1.pop(i)
-        return list1
+        return list1'''
 
 
 
@@ -106,18 +135,106 @@ class Set:
 class SC(Set):
     def __init__(self, iterable):
         super().__init__(iterable)
+        self.add(Set())
         for i in self:
             if type(i) != Set:
                 raise TypeError("Invalid agruments: One of the arguments was not a set.")
-        Is_valid = []
+        self.dim_simpl = {}
         for i in self:
-            Is_valid.append(False)
-        for i in self:
-            if not(i.issubsetof(self)):
-                raise TypeError(f"Invalid arguments: The element {i} is not a subset of a")
-    
-
+            try:
+                self.dim_simpl[len(i)-1].append(i)
+            except KeyError:
+                self.dim_simpl[len(i)-1] = [i]
+        self.highest_dim  = 0
+        while self._IsIn(self.highest_dim):
+            self.highest_dim += 1
+        self.highest_dim -= 1
         
+    def _IsIn(self,x):
+        try:
+            self.dim_simpl[x]
+            return True
+        except KeyError:
+            return False
+    
+    def border(self, set2: Set):
+        list1 = []
+        if not(self.isin(set2)):
+            return None
+        else:
+            for i in self.dim_simpl[len(set2)-2]:
+                if i.issubsetof(set2):
+                    list1.append(i)
+        return list1
+
+    def Define_graphical_cords(self, r = (100,101)):
+        self.cords = {}
+        self.cords[self.dim_simpl[0][0]] = [random.randint(200,500),random.randint(400,600)]
+        h1 = self.cords[self.dim_simpl[0][0]]
+        try:
+            angle = random.randint(361,581)/100
+            '''for some reason when the angles where smalller or larger then this range it gave a bad display'''
+            print(angle)
+            vectpr_angle = [math.cos(angle),math.sin(angle)]
+            self.cords[self.dim_simpl[0][1]] = [100*vectpr_angle[0]+h1[0],100*vectpr_angle[1]+h1[1]]
+            h2 = self.cords[self.dim_simpl[0][1]]
+        except:
+            return None
+        n = len(self.dim_simpl[0])
+        for i in self.dim_simpl[0]:
+            #pass
+            print(i)
+        for i in range(2,len(self.dim_simpl[0])):
+            #print(self.dim_simpl[0][i-2])
+            mif_point = [(h1[0] + h2[0])/2 , (h1[1] + h2[1])/2]
+            vector1 = [pow(-1,i)*r[0]/pow(pow((h1[1]-h2[1])/(h1[0]-h2[0]),2)+1,1/2)*(-(h1[1]-h2[1])/(h1[0]-h2[0])) , pow(-1,i)*r[0]/pow(pow((h1[1]-h2[1])/(h1[0]-h2[0]),2)+1,1/2)]
+            for n in range(2):
+                mif_point[n] += vector1[n]
+            self.cords[self.dim_simpl[0][i]] = mif_point
+            h1 = h2
+            h2 = self.cords[self.dim_simpl[0][i]]
+            #print(self.dim_simpl[0][i],h1,h2)
+
+    def Simplisial_Collaps(self) -> list[Set]: #-> SC with one simplitial chollaps performed
+        final = []
+        for i in self.dim_simpl[self.highest_dim]:
+            print(i)
+            remove = []
+            nmin1simp = self.border(i)
+            for n in nmin1simp:
+                is_valid = True
+                print(n)
+                for h in self.dim_simpl[self.highest_dim]:
+                    print(h)
+                    if n in self.border(h) and n != [] and h != i:
+                        is_valid = False
+                    if not(is_valid):
+                        break
+                if not(is_valid):
+                    break
+                else:
+                    remove = [n,i]
+                if remove != []:
+                    break
+            if remove != []:
+                break
+        print(final)
+        for i in self.element():
+            if not(i in remove):
+                final.append(i)
+        for i in final:
+            print(str(i)+", ",end="")
+        print()
+        return final
+
+
+            
+
+
+
+
+
+
     def __str__(self):
         return super().__str__() 
     def __iter__(self):
@@ -127,9 +244,12 @@ class SC(Set):
 
             
 
+if __name__ == "__main__":
+    g = Set([1,2,2,3])
+    f = Set()
+    h = SC([Set([1]),Set([2]),Set([1,2]),Set([3]),Set([3,2]),Set([1,3]),Set([1,2,3]),Set([1,4]),Set([4,2])])
 
-g = Set([1,2,2,3])
-f = Set([1,3,4])
-h = Set([Set([1]),Set([2])])
-print(g.power_set())
+
+    print(h.border(Set([1,4]))[0])
+
 
